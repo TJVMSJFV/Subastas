ef08603f92feba8af92f2ae98a5ffaa877dbcc1b
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-02-28 12:52:24 -0400

    refactor(base):  🎨  Cambio de Mongoose a Prisma
    - Se cambio todos los modelos de mongoose a modelos de prisma

--- lib/enums/gender.ts
+++
@@ -1,8 +0,0 @@
-// Si, hice un enum para el genero, no se sabe si alguien quiere identificarse como helicoptero de ataque tipo apache
-
-enum Gender {
-  MALE = "M",
-  FEMALE = "F",
-}
-
-export default Gender;
