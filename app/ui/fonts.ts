511cd77ac5c2efcd97432237621f47e54711586f
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-04-01 18:02:39 -0400

    chore: Using font in all the pages

--- app/ui/fonts.ts
+++
@@ -1,7 +0,0 @@
-import { Montserrat, Nixie_One } from 'next/font/google'
-
-export const montserrat = Montserrat({ subsets: ['latin']})
-export const nixieOne = Nixie_One({
-    subsets: ['latin'],
-    weight: '400'
-})
